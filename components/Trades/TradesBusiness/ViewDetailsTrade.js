3c7c2a14820eb0ff9d7e0c83afb201f535bb14d5
jean <ullauri_jeanalex@gmail.com>
2022-02-21 22:43:33 -0500

    fix

--- components/Trades/TradesBusiness/ViewDetailsTrade.js
+++ components/Trades/TradesBusiness/ViewDetailsTrade.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import TextField from '@material-ui/core/TextField';
 
